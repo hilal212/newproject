bf6a67c10e408a18735ef55216dd749b15684156
Elsa Hilal Avci <psk.hilal21@hotmail.com>
2022-03-15 10:31:10 -0400

    first commit

---
+++ newproject/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  newproject
+//
+//  Created by hilal on 3/14/22.
+//
+
+import UIKit
+import Parse
+
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+          
+          
+          let parseConfig = ParseClientConfiguration {
+              
+                  $0.applicationId = "2WKA3OQ0qOPVKp1FlJ5chZZJOrUdMDIEP1fytx8V"
+                  $0.clientKey = "3arSr7nLbvP6uMx4O6yCqBRqwjD6xYfMzqXkYs0w"
+                  $0.server = "https://parseapi.back4app.com"
+          }
+          
+          Parse.initialize(with: parseConfig)
+          
+          return true
+      }
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ newproject/PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  newproject
+//
+//  Created by hilal on 3/14/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+       
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    
+
+}
